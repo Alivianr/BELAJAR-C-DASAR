a590a513beec1c58d6cc5118db6fd447b02813cc
Alivian risqy <100820638+Alivianr@users.noreply.github.com>
2022-03-12 20:45:38 +0700

    Add files via upload

---
+++ deklarasitgl.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout << "••••BIODATA DIRI••••" <<endl;
+    cout << "masukkan tanggal lahir kamu : " <<endl;
+    cin >> a;
+    cout << "masukkan tahun lahir kamu : " <<endl;
+    cin >> b;
+    cout << "masukkan bulan lahir kamu : " <<endl;
+    cin >> c;
+    cout << "*•anda lahir tanggal : " <<endl;
+    cout << a <<endl;
+    cout << "*•anda lahir tahun : " <<endl;
+    cout << b <<endl;
+    cout << "*•anda lahir bulan : " <<endl;
+    cout << c <<endl;
+    cin.get();
+    return 0;
+}
